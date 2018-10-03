48933e17c226a0aae0cb22e4e78502d6f6c2c0aa
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-03 18:43:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  Handlebars.registerPartial(
+    'authorPartial', document.getElementById('author-partial-template').innerHTML
+  );
+});
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
@@ -12,11 +18,3 @@ function showRepositories(event, data) {
   const repoList = template(repos);
   document.getElementById('repositories').innerHTML = repoList;
 }
-
-document.addEventListener('DOMContentLoaded',
-function(event) {
-  Handlebars.registerPartial(
-    'authorPartial',
-    document.getElementById('author-partial-template').innerHTML;
-  );
-});
